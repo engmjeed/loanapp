1b4190700e7dd33d79d1791e6ee7982658a34927
Joshua Abuga <joshuabuga@gmail.com>
2021-09-12 03:31:38 +0300

    updates

--- products/admin.py
+++ products/admin.py
@@ -8,7 +8,17 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','short_name','minimum_principal','maximum_principal','interest_rate','max_repayment_months','fund','is_active',)
     # readonly_fields = ('name',)
     link_display = ('name',)
-    fields = ('name','short_name','minimum_principal','maximum_principal','interest_rate','max_repayment_months','fund','is_active',)
+    # fields = ('name','short_name','minimum_principal','maximum_principal','interest_rate','max_repayment_months','fund','is_active',)
+
+    list_filter = ('is_active',)
+    preserve_filters = False
+    
+    fieldsets = (
+        (None, {'fields': ('name','short_name','minimum_principal','maximum_principal','interest_rate','max_repayment_months',)}),
+        ('Applied Charges', {'fields': ('charges',)}),
+        ('Linked Fund', {'fields': ('fund',)}),
+    
+    )
 
     def has_delete_permission(self, request, obj=None):
         return False
